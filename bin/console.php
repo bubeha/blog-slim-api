734a05639734be4bd0964a7ae25bb977b53be640
bubeha <a.bubeshko@gmail.com>
2020-12-10 05:20:30 +0300

    add static analyzers and tools for code styles. add phpunit

--- bin/console.php
+++ bin/console.php
@@ -1,7 +1,10 @@
 <?php
 
+declare(strict_types = 1);
+
 use Symfony\Component\Console\Application;
 
+/** @psalm-suppress MissingFile */
 require_once __DIR__ . '/../vendor/autoload.php';
 
 $app = new Application();
