2ebdad7a55fd4520d21b9fa088ed151aeaa38f04
Alexandr Bubeshko <a.bubeshko@itransition.com>
2021-04-17 23:41:24 +0300

    fix psalm and logger

--- config/packages/logger.php
+++ config/packages/logger.php
@@ -4,6 +4,6 @@ declare(strict_types=1);
 
 return [
     'debug' => env('APP_DEBUG', false),
-    'file' => dirname(__DIR__) . '/var/logs/app.log',
+    'file' => dirname(__DIR__) . '/../var/logs/app.log',
     'stderr' => true,
 ];
--- public/index.php
+++ public/index.php
@@ -4,7 +4,6 @@ declare(strict_types=1);
 
 use App\Kernel;
 
-/** @var \Psr\Container\ContainerInterface $container */
 $container = require dirname(__DIR__) . '/config/bootstrap.php';
 
 $kernel = new Kernel($container);
