ec761d940367675d89c634e20cc4898b39df320d
Alexandr Bubeshko <a.bubeshko@itransition.com>
2021-04-18 16:30:32 +0300

    remove extra routes

--- config/router.php
+++ config/router.php
@@ -3,17 +3,8 @@
 declare(strict_types=1);
 
 use App\Http\Actions\Greeting;
-use App\Http\Responses\JsonResponse;
-use App\Services\Router\StaticRouterGroup as Group;
 use Slim\Interfaces\RouteCollectorProxyInterface;
 
 return static function (RouteCollectorProxyInterface $router): void {
     $router->get('/', Greeting::class);
-
-    $router->group(
-        '/test',
-        new Group(static function (RouteCollectorProxyInterface $router): void {
-            $router->get('', static fn () => new JsonResponse('hello vlad, i love your cock'));
-        })
-    );
 };
