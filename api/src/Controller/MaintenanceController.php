6bff1ade6fac94bbcdd702cce304fdd38c46c50c
Alexandr Bubeshko <a.bubeshko@gmail.com>
2021-05-24 23:53:16 +0300

    [feat] update by php cs fixed and update psalm cache directory

--- api/src/Controller/MaintenanceController.php
+++ api/src/Controller/MaintenanceController.php
@@ -5,16 +5,16 @@ declare(strict_types=1);
 namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
-class MaintenanceController
+final class MaintenanceController
 {
     /**
      * @Route(path="/", name="mainenance", methods={"GET"})
      */
     public function index(): Response
     {
-        return new JsonResponse("Maintenance mode");
+        return new JsonResponse('Maintenance mode');
     }
 }
